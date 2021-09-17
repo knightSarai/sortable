0f0e37b35b40a12f7d06458dec122a6f5b2ca3d4
knight <knight.sarai.dev@gmail.com>
2021-09-18 01:26:17 +0300

    Implement buble sort

--- build/index.js
+++ build/index.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("KngihTscript Change");
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                var leftHand = this.collection[j];
+                var rightHand = this.collection[j + 1];
+                if (leftHand > rightHand) {
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+// Makethis alghorithem to work with almost any sort of iterable
+// Change the way we're doing comparaision, becuase of the string
+// Handle swaping in different way. *Strings are immutable
+var collection = [10, 3, -5, 0];
+var sorter = new Sorter(collection);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("KngihTscript Change")
+class Sorter {
+    constructor(public collection: number[]){}
+
+    sort(): void {
+        const {length} = this.collection;
+        for (let i = 0; i < length; i++) {
+            for(let j=0; j < length - i -1; j++){
+                const leftHand = this.collection[j];
+                const rightHand = this.collection[j + 1]
+               if (leftHand > rightHand){
+                this.collection[j] = this.collection[j + 1] 
+                this.collection[j + 1] = leftHand;
+               }
+            }
+        }
+    }
+}
+
+// Makethis alghorithem to work with almost any sort of iterable
+// Change the way we're doing comparaision, becuase of the string
+// Handle swaping in different way. *Strings are immutable
+
+const collection = [10, 3, -5, 0]
+const sorter = new Sorter(collection);
+sorter.sort()
+console.log(sorter.collection);
